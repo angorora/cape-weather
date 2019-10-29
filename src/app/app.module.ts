f8131875da9654ebca6df4812f52e0d9ffb2bca2
angorora <allen.ngorora@gmail.com>
2019-10-29 17:36:10 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+/* Framework specific */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/* App specific */
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './core/landing-page/landing-page.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
