d90e7a7428363248460c51a0e30c0d1d44a309ae
angorora <allen.ngorora@gmail.com>
2019-11-02 19:46:59 +0200

    Deployment touches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,9 @@ import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './core/components/landing-page/landing-page.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { MiniSummaryComponent } from './shared/mini-summary/mini-summary.component';
-import { FullSummaryComponent } from './shared/full-summary/full-summary.component';
-import { AlertComponent } from './shared/alert/alert.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /* Material Design Components */
-import {MatCardModule} from '@angular/material/card';
-import {MatListModule} from '@angular/material/list'; 
-import {MatIconModule} from '@angular/material/icon'; 
-import {MatTabsModule} from '@angular/material/tabs'; 
 import {MatButtonModule} from '@angular/material/button';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -25,30 +18,21 @@ import { ForecastService } from './services/forecast.service';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EffectsModule } from '@ngrx/effects';
-import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
-    MiniSummaryComponent,
-    FullSummaryComponent,
-    AlertComponent
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatCardModule,
-    MatListModule,
-    MatIconModule,
     MatButtonModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     StoreModule.forRoot(reducers, { metaReducers }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    EffectsModule.forRoot([AppEffects])
   ],
   providers: [ForecastService,{ provide: HTTP_INTERCEPTORS, useClass: CachingInterceptor, multi: true }],
   bootstrap: [AppComponent]
